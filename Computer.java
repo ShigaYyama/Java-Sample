c02d9b3463840c2ff97a6df906fd05fb1769f622
Taito <13.taito@gmail.com>
2022-02-09 20:42:00 +0900

    変更を反映

--- Computer.java
+++ Computer.java
@@ -2,28 +2,26 @@ package kadai6to2;
 
 public class Computer {
 	String comHand;
-	
-	
+
 	public int Action() {
 		int ran = new java.util.Random().nextInt(3);
 		return ran;
 	}
-	
+
 	public String Hand(int handNum) {
-		String[] hand = { "グー", "チョキ", "パー" };	
-		switch (handNum){
-			case 0:
-				comHand = hand[0];
-				break;
-			case 1:
-				comHand = hand[1];
-				break;
-			case 2:
-				comHand = hand[2];
-				break;
+		String[] hand = { "グー", "チョキ", "パー" };
+		switch (handNum) {
+		case 0:
+			comHand = hand[0];
+			break;
+		case 1:
+			comHand = hand[1];
+			break;
+		case 2:
+			comHand = hand[2];
+			break;
 		}
 		return comHand;
 	}
-
 
 }
--- User.java
+++ User.java
@@ -3,41 +3,37 @@ package kadai6to2;
 public class User {
 	String userHand;
 	int num;
-	
-	public User() {
-	}
-	
-	public int Action() {
-		
-	while(true) {
-		@SuppressWarnings("resource") 
-		String numStr = new java.util.Scanner(System.in).nextLine();
-		
 
-		if (numStr == "0" || numStr == "1" || numStr == "2") {
-			num = Integer.parseInt(numStr);
-			break;
-		}else {
-			System.out.println("正しい値が入力されませんでした。入力を続けてください。");
-			continue;
+	public int Action() {
+
+		while (true) {
+			@SuppressWarnings("resource")
+			int numStr = new java.util.Scanner(System.in).nextInt();
+
+			if (numStr == 0 || numStr == 1 || numStr == 2) {
+				num = numStr;
+				break;
+			} else {
+				System.out.println("正しい値が入力されませんでした。入力を続けてください。");
+				continue;
+			}
 		}
+
+		return num;
 	}
-	
-	return num;
-	}
-	
+
 	public String Hand(int handNum) {
 		String[] hand = { "グー", "チョキ", "パー" };
-		switch (handNum){
-			case 0:
-				userHand = hand[0];
-				break;
-			case 1:
-				userHand = hand[1];
-				break;
-			case 2:
-				userHand = hand[2];
-				break;
+		switch (handNum) {
+		case 0:
+			userHand = hand[0];
+			break;
+		case 1:
+			userHand = hand[1];
+			break;
+		case 2:
+			userHand = hand[2];
+			break;
 		}
 		return userHand;
 	}
